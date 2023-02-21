de1f5903f3f86ba4c4e434c14e9b600582c76ce6
Diana Murtazina <murka8831@yandex.ru>
2023-02-21 16:08:47 +0400

    Change position by click

--- static/js/index.js
+++ static/js/index.js
@@ -22,6 +22,22 @@ document.getElementById("shuffle").addEventListener("click", () => {
 });
 
 /** 3.Change position by click */
+const blankNumber = 16;
+containerNode.addEventListener("click", (event) => {
+    const buttonNode = event.target.closest("button");
+    if (!buttonNode) {
+        return;
+    }
+    const buttonNumber = Number(buttonNode.dataset.matrixId);
+    const buttonCoords = findCoordinatesByNumber(buttonNumber, matrix);
+    const blankCoords = findCoordinatesByNumber(blankNumber, matrix);
+    const isValid = isValidForSwap(buttonCoords, blankCoords);
+    if (isValid) {
+        swap(buttonCoords, blankCoords, matrix);
+        setPositionItems(matrix);
+    }
+});
+
 /** 4.Change position by keydown */
 /** 5.Show won */
 
@@ -66,4 +82,28 @@ function shuffleArray(arr) {
         .map((value) => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value);
+}
+
+function findCoordinatesByNumber(number, matrix) {
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] === number) {
+                return { x, y };
+            }
+        }
+    }
+    return null;
+}
+
+function isValidForSwap(coords1, coords2) {
+    const diffX = Math.abs(coords1.x - coords2.x);
+    const diffY = Math.abs(coords1.y - coords2.y);
+
+    return (diffX === 1 || diffY === 1) && (coords1.x === coords2.x || coords1.y === coords2.y);
+}
+
+function swap(coords1, coords2, matrix) {
+    const coords1Number = matrix[coords1.y][coords1.x];
+    matrix[coords1.y][coords1.x] = matrix[coords2.y][coords2.x];
+    matrix[coords2.y][coords2.x] = coords1Number;
 }
